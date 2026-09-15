2e23ca591157701815292b555a9f17ac51bc8f1f
ALEXANDRE GULINI <alexandre_gulini@estudante.sesisenai.org.br>
2026-09-15 14:14:24 -0300

    Correção na Máscara de pesquisa

--- base.php
+++ base.php
@@ -39,7 +39,8 @@ if ($base_path === '//') { $base_path = '/'; }
         <!-- Depois: transformado em formulário -->
         <form class="search-bar" action="<?= $base_path ?>views/recipes/recipe_list.php" method="GET">
             
-            <input type="text" name="q" placeholder="Buscar receitas..." value="<?= htmlspecialchars($_GET['q'] ?? '') ?>"><i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" name="q" placeholder="Buscar receitas..." maxlength="50" value="<?= htmlspecialchars($_GET['q'] ?? '') ?>">
+            <i class="fa-solid fa-magnifying-glass"></i>
             
             <!-- botão invisível para permitir submit com Enter -->
             <button type="submit"></button>
